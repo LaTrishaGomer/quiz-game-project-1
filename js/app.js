e94a51da062a64fbf4001c2823899618c3b39745
latrisha <l_gomer@yahoo.com>
2025-02-24 14:45:53 -0800

    create user story for js logic

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+/*-------------------------------- Design Credit: slabdsgn --------------------------------*/
+// Commercial License obtained via Envato Elements: https://elements.envato.com/fashion-fashion-landing-page-RCJUDKN
+
+
+
 /*-------------- Constants -------------*/
 
 
@@ -12,3 +17,13 @@
 
 /*----------- Event Listeners ----------*/
 
+
+
+// User story for Throwback Quiz game:
+
+// As a user, I want to see a landing page when I arrive at the website to know I’m in the right place.
+// As a user, I want to see clearly labeled buttons for each category on the landing page, so I know my options.
+// As a user, I want to see each of my quiz options and know how much time I have to answer each question.
+// As a user, I want visual feedback after selecting, so I know my choice has been registered.
+// As a user, I want to see a clear message indicating if I picked the correct answer so that I can understand the outcome.
+// As a user, I want to play another round or have the option to choose another category.
