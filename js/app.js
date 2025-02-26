d29e56f05bcc7858e62316de10fd519ed448ff7f
latrisha <l_gomer@yahoo.com>
2025-02-25 16:54:29 -0800

    set up outline of quiz constants and variables

--- js/app.js
+++ js/app.js
@@ -5,9 +5,49 @@
 
 /*-------------- Constants -------------*/
 
+const questions [
+    {
+        category: "Totally 80s",
+        question:,
+        image: "imgs/categories-pacman.png",
+        options:,
+        answer:,
+    },
+
+    {
+        category: "Totally 80s", 
+        question:,
+        options:,
+        answer:,
+    },
+
+    {
+        category: "Totally 80s", 
+        question:,
+        options:,
+        answer:,
+    },
+
+    {
+        category: "Totally 80s",
+        question:,
+        options:,
+        answer:,
+    },
+
+    {
+        category: "Totally 80s",
+        question:,
+        options:,
+        answer:,
+    },
+];
+
 
 /*---------- Variables (state) ---------*/
 
+let currentQuestion = 0;
+let score = 0;
 
 /*----- Cached Element References  -----*/
 
@@ -18,12 +58,3 @@
 /*----------- Event Listeners ----------*/
 
 
-
-// User story for Throwback Quiz game:
-
-// As a user, I want to see a landing page when I arrive at the website to know I’m in the right place.
-// As a user, I want to see clearly labeled buttons for each category on the landing page, so I know my options.
-// As a user, I want to see each of my quiz options and know how much time I have to answer each question.
-// As a user, I want visual feedback after selecting, so I know my choice has been registered.
-// As a user, I want to see a clear message indicating if I picked the correct answer so that I can understand the outcome.
-// As a user, I want to play another round or have the option to choose another category.
