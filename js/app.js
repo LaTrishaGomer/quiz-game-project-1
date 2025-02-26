2d1740c851ba7548f3917f0a467184dba8998a99
latrisha <l_gomer@yahoo.com>
2025-02-26 12:47:47 -0800

    define constants and variables. add test questions

--- js/app.js
+++ js/app.js
@@ -1,46 +1,25 @@
 /*-------------------------------- Design Credit: slabdsgn --------------------------------*/
-// Commercial License obtained via Envato Elements: https://elements.envato.com/fashion-fashion-landing-page-RCJUDKN
+// Commercial License for Figma design file obtained via Envato Elements: https://elements.envato.com/fashion-fashion-landing-page-RCJUDKN
 
+//Photos & Assets: I do not own nor claim to own the rights to any images or photos shared in this class project
 
 
 /*-------------- Constants -------------*/
 
-const questions [
+const quizQuestions = [
     {
-        category: "Totally 80s",
-        question:,
-        image: "imgs/categories-pacman.png",
-        options:,
-        answer:,
+        question: "What was the name of the fictional HBCU where A Different World was set?",
+        image: "imgs/q1-80s-different-world.jpg",
+        options:["Hillman College", "Howard University","Herbert College", "Douglas College"],
+        correctAnswer: 0
     },
 
     {
-        category: "Totally 80s", 
-        question:,
-        options:,
-        answer:,
-    },
-
-    {
-        category: "Totally 80s", 
-        question:,
-        options:,
-        answer:,
-    },
-
-    {
-        category: "Totally 80s",
-        question:,
-        options:,
-        answer:,
-    },
-
-    {
-        category: "Totally 80s",
-        question:,
-        options:,
-        answer:,
-    },
+        question: "What is the name of this popular Nintendo video game?",
+        image: "imgs/q2-80s-duck-hunt.jpg",
+        options:["Duck Tails", "Duck Hunt", "Donald The Duck", "Duck Adventures"],
+        correctAnswer: 1
+    }
 ];
 
 
@@ -49,10 +28,19 @@ const questions [
 let currentQuestion = 0;
 let score = 0;
 
+
 /*----- Cached Element References  -----*/
 
+const category = document.getElementById("category");
+const questionNumber = document.getElementById("quiz-question-count");
+const question = document.getElementById("question");
+const image = document.getElementById("quiz-image");
+const options = document.querySelectorAll(".option");
+const nextButton = document.getElementById("next-button");
+const result = document.getElementById("result");
 
 /*-------------- Functions -------------*/
+
 
 
 /*----------- Event Listeners ----------*/
